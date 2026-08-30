904329870348db8af7ff2a47e634c7feb751b97d
Vinod Rajbhar <vinodraj12390@gmail.com>
2026-08-30 17:59:52 +0530

    Add chnages file

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
 <section id="center">
-   <h1>Welcome to my website!
+   <h1>Thank you for comeback.Welcome to my website!
 </section>
 
 <div class="ticks"></div>
